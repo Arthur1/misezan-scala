570acf1fedd5ee78b07021a5ce1ac869f2787bd3
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2024-02-26 23:32:28 +0900

    min, maxを演算子と見る

--- src/main/scala/com/github/arthur1/misezan/Misezan.scala
+++ src/main/scala/com/github/arthur1/misezan/Misezan.scala
@@ -2,7 +2,7 @@ package io.github.arthur1.misezan
 
 extension (i: Int)
   def mise(j: Int): BigDecimal = {
-    (i.min(j), i.max(j)) match {
+    (i min j, i max j) match {
       case (6, 9)       => 11
       case (2, 5)       => 1.1
       case (1, 100)     => 83
